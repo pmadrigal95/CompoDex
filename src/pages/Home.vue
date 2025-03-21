a0f178f4a71d60b3ee13fdc5b376b7e9a1fcf2cd
Pablo Madrigal <pablomadrigal.1995@gmail.com>
2025-03-20 23:25:10 -0600

    initial config

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,14 @@
 <template>
     <div>
-      <h1>Home Page</h1>
+      <h1>Counter: {{ counter.count }}</h1>
+      <q-btn @click="counter.increment" color="primary" label="Increment" />
+      <q-btn @click="counter.decrement" color="negative" label="Decrement" class="q-ml-sm" />
     </div>
   </template>
+  
+  <script setup>
+  import { useCounterStore } from '@/stores/counterStore';
+  
+  const counter = useCounterStore();
+  </script>
+  
